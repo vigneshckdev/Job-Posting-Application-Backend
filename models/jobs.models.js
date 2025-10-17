32b67c2bca013ae84d7ae6f2e84aea8e59dc0d7a
vigneshckdev <vigneshckdev@gmail.com>
2025-10-17 12:01:17 +0530

    changes in the schema

--- models/jobs.models.js
+++ models/jobs.models.js
@@ -1,35 +1,39 @@
 const mongoose = require("mongoose");
-const jobSchema = new mongoose.Schema({
+const jobSchema = new mongoose.Schema(
+  {
     jobTitle: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     companyName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     location: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     jobType: {
-        type: String,
-        enum: [
-  "Full-Time(On-site)",
-  "Part-Time(On-site)",
-  "Full-Time(Remote)",
-  "Part-Time(Remote)"
-]
+      type: String,
+      enum: [
+        "Full-Time(On-site)",
+        "Part-Time(On-site)",
+        "Full-Time(Remote)",
+        "Part-Time(Remote)",
+      ],
+      required: true,
     },
     jobDescription: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     jobQualification: {
-        type: [String],
-        required: true
-    }
-}, {timestamps: true})
+      type: [String],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 const Jobs = mongoose.model("Jobs", jobSchema);
 module.exports = Jobs;
